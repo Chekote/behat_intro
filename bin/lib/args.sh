70388e973fc77da609e46d9bc362939d69b4bc3d
Chekote <domhnall.tilliere@gmail.com>
2016-06-01 15:04:13 -0500

    Initial commit.
    Added basic configuration for a Behat tested project.

---
+++ bin/lib/args.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# (re)add double quotes around arguments
+ARGS=''
+for i in "$@"; do
+    ARGS="$ARGS \"${i//\"/\\\"}\""
+done
---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,9 @@
+<?php
+
+use Behat\MinkExtension\Context\MinkContext;
+
+/**
+ * Defines step implementations from the context of a Web Page.
+ */
+class FeatureContext extends MinkContext {
+}

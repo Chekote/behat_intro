ac2d5e839abfb97b23bee95b8cdd972a8eebab61
Donald Tyler <domhnall.tilliere@gmail.com>
2016-07-06 15:39:05 -0500

    Can set name on Behat created Product.

--- features/contexts/domain/ProductContext.php
+++ features/contexts/domain/ProductContext.php
@@ -1,6 +1,7 @@
 <?php namespace contexts\domain;
 
 use Behat\FlexibleMink\PseudoInterface\StoreContextInterface;
+use Exception;
 use model\Product;
 
 trait ProductContext {
@@ -8,13 +9,28 @@ trait ProductContext {
     use StoreContextInterface;
 
     /**
-     * OK method, but not very flexible. I can't specify the property values;
+     * Better method. Can specify name. But pattern is getting complex now.
+     * Also, we can end up with more products than we need, because there
+     * might already be a product matching the requirements.
      *
-     * @Given I have a Product
+     * @Given /^I have a Product(?: with)?(?: the name "(?P<name>[^"]+)")?$/
+     * @param string $name The name for the product.
      */
-    public function assertProductExists() {
+    public function assertProductExists($name = 'Blox') {
         $product = new Product();
+        $product->name = $name;
 
         $this->put($product, 'Product');
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @Then the Product should have the name :name
+     */
+    public function assertProductName($name) {
+        if ($name != $this->getThingProperty('Product', 'name')) {
+            throw new Exception('Property did not match');
+        }
     }
 }
--- features/product.feature
+++ features/product.feature
@@ -6,3 +6,8 @@ Feature: Product
   Scenario: Product!
     Given I have a Product
      Then the store should contain something under key "Product"
+
+  Scenario: Product with name
+    Given I have a Product with the name "Boop Shoops"
+     Then the store should contain something under key "Product"
+      And the Product should have the name "Boop Shoops"
